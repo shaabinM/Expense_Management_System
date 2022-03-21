7c8bf89c2dde5feaa40b74bf359566fcb5ca57f0
ShaabinM <m.shaabin@gmail.com>
2022-03-21 14:12:43 +0530

    Feedback-mail form completed

--- app/src/main/java/com/shaabin_m/login_main/feedback.java
+++ app/src/main/java/com/shaabin_m/login_main/feedback.java
@@ -2,13 +2,49 @@ package com.shaabin_m.login_main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class feedback extends AppCompatActivity {
+    private EditText mEditTextSubject, mEditTextMessage, mEditTextTo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        mEditTextSubject = findViewById(R.id.edit_text_Subject);
+        mEditTextMessage =findViewById(R.id.edit_text_message);
+        mEditTextTo = findViewById(R.id.edit_text_to);
+        MaterialButton buttonSend = findViewById(R.id.button_feedback);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail( );
+            }
+        });
+
+    }
+    private void sendMail(){
+
+
+        String recipient = mEditTextTo.getText().toString();
+        String[] recipients = recipient.split(",");
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent,"choose an Email client"));
     }
 }
